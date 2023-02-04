a034c0b5028600840810c9590a721911d390aa69
Korrakot <korrakot.intanon@impact360design.com>
2023-02-04 13:33:07 +0700

    redo the branch mech

--- Assets/Branch.cs
+++ Assets/Branch.cs
@@ -1,49 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static BranchController;
 
 public class Branch : MonoBehaviour
 {
-    [HideInInspector]
-    public BranchController parent;
-
-    [Header("Obj Ref")]
-    public LineRenderer lineRenderer;
-
-    [Header("Properties")]
-    public Vector2 origin;
-    public List<int> branchVerticesIndex = new List<int>();
-    public List<BranchVertex> branchVertices = new List<BranchVertex>();
-
-    private void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        lineRenderer = GetComponent<LineRenderer>();
+        
     }
 
-    private void Start()
+    // Update is called once per frame
+    void Update()
     {
+        
     }
-
-    public void getBranchVertices()
-    {
-        branchVertices.Clear();
-        for (int i = 0; i < branchVerticesIndex.Count; i++)
-        {
-            branchVertices.Add(parent.depthVertices[i][branchVerticesIndex[i]]);
-        }
-    }
-
-    public void updateLineRenderer()
-    {
-        lineRenderer.positionCount = branchVertices.Count +1 ;
-
-        lineRenderer.SetPosition(0, origin);
-
-        for (int i = 0; i < branchVertices.Count; i++)
-        {
-            lineRenderer.SetPosition(i+1, branchVertices[i].pos);
-        }
-    }
-
 }
